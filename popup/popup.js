e5bd76b9fec0ddc2bb03c946ba02e49ae95abd51
Alex <107483671+AlexGallego2005@users.noreply.github.com>
2025-09-08 00:25:09 +0200

    history

--- popup/popup.js
+++ popup/popup.js
@@ -1,12 +1,101 @@
-const passdiv = document.getElementById('password');
-const gibberish = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','0','1','2','3','4','5','6','7','8','9','!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^', '_', '`', '{', '|', '}', '~'];
-const slider = document.getElementById("length");
-const mLength = document.getElementById('lengthNum');
-mLength.value = slider.value;
-var upper = true;
-var lower = true;
-var number = true;
-var symbol = true;
+function UpdateSlider()
+{
+    const slider = document.getElementById('length');
+    const number = document.getElementById('lengthNum');
+
+    number.value = slider.value;
+    const min = slider.min || 5;
+    const max = slider.max || 30;
+    const val = ((slider.value - min) * 100) / (max - min);
+
+    slider.style.setProperty('--value', val + '%');
+
+    chrome.storage?.local?.get(['settings'], (res) => {
+        /** @type { AppParams } */
+        const params = {
+            settings: res.settings
+        };
+        
+        params.settings.length = slider.value;
+
+        chrome.storage.local.set({ settings: params.settings });
+
+        ReloadPassword();
+    });
+};
+
+function UpdateSliderFromNumber()
+{
+    const slider = document.getElementById('length');
+    const number = document.getElementById('lengthNum');
+
+    slider.value = number.value;
+    UpdateSlider();
+};
+
+function ReloadPassword()
+{
+    const gibberish = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+        'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+        '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+        '!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^', '_', '`', '{', '|', '}', '~'];
+
+    chrome.storage?.local?.get(['passwords', 'settings'], (res) => {
+        /** @type { AppParams } */
+        const params = {
+            passwords: res.passwords || new Array(),
+            settings: res.settings
+        };
+
+        const passwordBox = document.getElementById('password');
+        passwordBox.innerHTML = '';
+
+        if (
+            !params.settings.charset.upper &&
+            !params.settings.charset.lower &&
+            !params.settings.charset.number &&
+            !params.settings.charset.symbol
+        )
+        {
+            return passwordBox.textContent = '>:(';
+
+            /*document.getElementById('upper').disabled = true;
+            document.getElementById('lower').disabled = true;
+            document.getElementById('number').disabled = true;
+            document.getElementById('symbol').disabled = true;
+            document.getElementById('length').disabled = true;
+            document.getElementById('lengthNum').disabled = true;
+            document.getElementById('reload').disabled = true;
+            document.getElementById('copy').disabled = true;*/
+        };
+
+        for (var i = 0; i < params.settings.length; i++)
+        {
+            const char = gibberish.at(Math.floor(Math.random() * gibberish.length));
+            if (
+                (char.match(/[A-Z]/) && !params.settings.charset.upper) ||
+                (char.match(/[a-z]/) && !params.settings.charset.lower) ||
+                (char.match(/\d/) && !params.settings.charset.number) ||
+                (char.match(/[$-/:-?{-~!"^_`\[\]@\\#]/) && !params.settings.charset.symbol)
+            )
+                i--;
+            else
+                passwordBox.insertAdjacentHTML('beforeend',
+                    `<span style="color: #${ char.match(/[a-zA-Z]/) ? '262626ff' : char.match(/\d/) ? '4f6dffff' : 'ff78beff' };">${ char }</span>`
+                );
+        };
+
+        const password = passwordBox.textContent;
+        if (password.length > 64) return;
+
+        params.passwords.unshift(password);
+        params.passwords = params.passwords.slice(0, 30);
+
+        chrome.storage.local.set({ passwords: params.passwords });
+
+        return;
+    });
+};
 
 /**
  * @typedef { object } AppSettingsCharset
@@ -15,111 +104,127 @@ var symbol = true;
  * @property { boolean } number
  * @property { boolean } symbol
  */
-
 /**
  * @typedef { object } AppSettings
  * @property { number } length Longitud de la contraseña
  * @property { AppSettingsCharset } charset
  */
-
 /**
  * @typedef { object } AppParams
  * @property { Array<string> } passwords
  * @property { AppSettings } settings
  */
-
+/**
+ * 
+ */
 function AppSetup()
 {
     chrome.storage?.local?.get(['passwords', 'settings'], (res) => {
         /** @type { AppParams } */
         const params = {
             passwords: res.passwords || new Array(),
-            settings: res.settings || {}
+            settings: res.settings || { length: 18, charset: { upper: true, lower: true, number: true, symbol: true } }
         };
 
-        const passwordLengthSlider = document.getElementById('length');
-        const passwordLengthManual = document.getElementById('lengthNum');
-        const upper = document.getElementById('upper');
-        const lower = document.getElementById('lower');
-        const number = document.getElementById('number');
-        const symbol = document.getElementById('symbol');
+        if (
+            !params.settings.charset.upper &&
+            !params.settings.charset.lower &&
+            !params.settings.charset.number &&
+            !params.settings.charset.symbol
+        ) params.settings.charset.upper = true;
 
-        passwordLengthSlider.max = params.settings.length;
-        passwordLengthManual.max = params.settings.length;
-        if (params.settings.charset.upper ?? true) upper.checked;
-        if (params.settings.charset.lower ?? false) lower.checked;
-        if (params.settings.charset.number ?? true) number.checked;
-        if (params.settings.charset.symbol ?? true) symbol.checked;
+        document.getElementById('length').value = params.settings.length;
+        document.getElementById('lengthNum').value = params.settings.length;
+        document.getElementById('upper').checked = params.settings.charset.upper;
+        document.getElementById('lower').checked = params.settings.charset.lower;
+        document.getElementById('number').checked = params.settings.charset.number;
+        document.getElementById('symbol').checked = params.settings.charset.symbol;
+
+        chrome.storage.local.set({ settings: params.settings });
+
+        UpdateSlider();
+        ReloadPassword();
     });
-};
 
-function reloadPassword(len=30)
-{
-    passdiv.textContent = '';
-    passdiv.innerHTML = '';
+    document.getElementById('lengthNum').addEventListener('input', () => UpdateSliderFromNumber());
+    document.getElementById('length').addEventListener('input', () => UpdateSlider());
 
-    if (!upper && !lower && !number && !symbol) return passdiv.textContent = 'Selecciona algo :(';
-
-    for (var i = 0; i < len; i++)
+    function changeCharset(charset)
     {
-        const char = gibberish.at(Math.floor(Math.random() * gibberish.length));
-        if ((char.match(/[A-Z]/) && !upper) || (char.match(/[a-z]/) && !lower) || (char.match(/\d/) && !number) || (char.match(/[$-/:-?{-~!"^_`\[\]@\\#]/) && !symbol))
-            i--;
-        else
-            passdiv.insertAdjacentHTML('beforeend', `<span style="color: #${ char.match(/[a-zA-Z]/) ? '262626ff' : char.match(/\d/) ? '4f6dffff' : 'ff78beff' };">${ char }</span>`);
+        chrome.storage?.local?.get(['settings'], (res) => {
+            /** @type { AppParams } */
+            const params = {
+                settings: res.settings
+            };
+            
+            params.settings.charset[charset] = !params.settings.charset[charset];
+            chrome.storage.local.set({ settings: params.settings });
+
+            ReloadPassword();
+        });
     };
-};
 
-function updateGradient()
-{
-    reloadPassword(slider.value);
-    mLength.value = slider.value;
-    const min = slider.min || 5;
-    const max = slider.max || 30;
-    const val = ((slider.value - min) * 100) / (max - min);
-    slider.style.setProperty("--value", val + "%");
-};
+    document.getElementById('upper').addEventListener('change', () => changeCharset('upper'));
+    document.getElementById('lower').addEventListener('change', () => changeCharset('lower'));
+    document.getElementById('number').addEventListener('change', () => changeCharset('number'));
+    document.getElementById('symbol').addEventListener('change', () => changeCharset('symbol'));
 
-function updateSlider()
-{
-    slider.value = mLength.value;
-    updateGradient();
-};
+    document.getElementById('history').addEventListener('click', () => {
+        const passwordHistory = document.getElementById('passHistory');
+        const passwordHistoryContent = document.getElementById('passHistoryContent');
 
-updateGradient();
+        chrome.storage?.local?.get(['passwords'], (res) => {
+            /** @type { AppParams } */
+            const params = {
+                passwords: res.passwords
+            };
+
+            for (const password of params.passwords)
+            {
+                console.log(password)
+                passwordHistoryContent.insertAdjacentHTML('afterbegin', `<div style="padding: 10px 15px; background-color: #54ddff11; border-radius: 10px;"><span>${ password }</span><span label="copy" class="material-symbols-outlined btn">content_copy</span></div>`);
+            };
+
+            passwordHistory.style.maxHeight = '250px';
+        });
+    });
+
+    return;
+};
 
 window.onload = function() {
     AppSetup()
-    reloadPassword(slider.value);
 
-    mLength.addEventListener('change', () => updateSlider());
     document.getElementById('uncap').addEventListener('click', () => {
-        if (slider.max === '30')
+        if (document.getElementById('length').max === '30')
         {
-            slider.setAttribute('max', '9999');
-            mLength.setAttribute('max', '9999');
+            document.getElementById('length').setAttribute('max', '128');
+            document.getElementById('lengthNum').setAttribute('max', '128');
+            document.getElementById('uncap').textContent = 128;
+        }
+        else if (document.getElementById('length').max === '128')
+        {
+            document.getElementById('length').setAttribute('max', '9999');
+            document.getElementById('lengthNum').setAttribute('max', '9999');
+            document.getElementById('uncap').textContent = 'sí.';
         }
         else
         {
-            slider.setAttribute('max', '30');
-            mLength.setAttribute('max', '30');
+            document.getElementById('length').setAttribute('max', '30');
+            document.getElementById('lengthNum').setAttribute('max', '30');
+            document.getElementById('uncap').textContent = 30;
         };
 
-        updateGradient();
+        UpdateSlider();
     });
-    document.getElementById('length').addEventListener('input', () => updateGradient());
-    document.getElementById('reload').addEventListener('click', () => reloadPassword(slider.value));
+    document.getElementById('reload').addEventListener('click', () => ReloadPassword());
     document.getElementById('copy').addEventListener('click', () => {
         document.getElementById('copy').style.opacity = 0;
         document.getElementById('copied').style.opacity = 1;
-        navigator.clipboard.writeText(passdiv.textContent);
+        navigator.clipboard.writeText(document.getElementById('password').textContent);
         setTimeout(() => {
             document.getElementById('copy').style.opacity = 1;
             document.getElementById('copied').style.opacity = 0;
         }, 300);
     });
-    document.getElementById('upper').addEventListener('change', () => { upper = !upper; reloadPassword(slider.value) });
-    document.getElementById('lower').addEventListener('change', () => { lower = !lower; reloadPassword(slider.value) });
-    document.getElementById('number').addEventListener('change', () => { number = !number; reloadPassword(slider.value) });
-    document.getElementById('symbol').addEventListener('change', () => { symbol = !symbol; reloadPassword(slider.value) });
 };
